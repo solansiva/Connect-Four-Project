2ed597dcfcd6279025cb9b5cbd9c922a393d25e2
solansiva <48026975+solansiva@users.noreply.github.com>
2019-02-26 12:44:05 -0500

    Add files via upload
    ConnectFourEnum is the enumerator for the Connect Four Game

---
+++ ConnectFourEnum.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * ConnectFourEnum contains the enumerator for ConnectFourGame
+ *
+ * @author Schramm
+ */
+public enum ConnectFourEnum {
+    IN_PROGRESS("Game in Progress"), RED("Red"), BLACK("Black"), DRAW("It's a draw!"), EMPTY(" ");
+
+    private String value;
+
+    ConnectFourEnum(String value) {
+        this.value = value;
+    }
+}
