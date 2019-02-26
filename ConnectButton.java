1bc44508c8b189b0f3fa76713c2e826309b20695
solansiva <48026975+solansiva@users.noreply.github.com>
2019-02-26 12:39:51 -0500

    Add files via upload
    A class that initializes the button for connect four, inherits button

---
+++ ConnectButton.java
@@ -0,0 +1,40 @@
+
+import javafx.scene.control.Button;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sumathy
+ */
+public class ConnectButton extends Button{
+    private int row;
+    private int column;
+
+    
+    public ConnectButton(String label,int row,int column){
+        super(label);
+        this.row = row;
+        this.column = column;
+    }
+    
+    public int getRow(){
+        return this.row;
+    }
+    
+    public int getColumn(){
+        return this.column;
+    }
+    
+    public String toString(){
+        return "(" + this.row + "," + this.column + ")"; 
+    }
+    
+}
